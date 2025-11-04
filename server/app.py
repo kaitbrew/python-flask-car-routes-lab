2add85c157edd80a8192b69f0928c1c2782ef851
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2025-11-03 19:25:07 -0500

    model route now functional, all tests passing

--- server/app.py
+++ server/app.py
@@ -11,4 +11,6 @@ def index():
 @app.route('/<model>')
 def model(model):
     if model in existing_models:
-        return existing_models
+        return f'Flatiron {model} is in our fleet!'
+    else:
+        return f'No models called {model} exists in our catalog'
